3fbe128ee125513e0b49789c137fd3ccdb203e94
Everton Cabral <96452540+EvertoonCabral@users.noreply.github.com>
2025-05-08 23:20:22 -0300

    feat: Criado Interface de repositorio de Produto

---
+++ ApiCatalago/Repositories/IProdutoRepository.cs
@@ -0,0 +1,16 @@
+﻿using ApiCatalago.Models;
+
+namespace ApiCatalago.Repositories
+{
+    public interface IProdutoRepository
+    {
+
+        IQueryable<Produto> GetProdutos();
+        Produto GetProduto(int id);
+        Produto CreateProdutos(Produto produto);
+        bool UpdateProduto(Produto produto);
+        bool DeleteProduto(int id);
+
+
+    }
+}
---
+++ ApiCatalago/Repositories/ProdutoRepository.cs
@@ -0,0 +1,51 @@
+﻿using ApiCatalago.Context;
+using ApiCatalago.Models;
+
+namespace ApiCatalago.Repositories
+{
+    public class ProdutoRepository : IProdutoRepository
+    {
+
+        private readonly AppDbContext _context;
+
+        public ProdutoRepository(AppDbContext context)
+        {
+            _context = context;
+        }
+
+        public IQueryable<Produto> GetProdutos()
+        {
+
+            var produtos = _context.Produtos;
+
+            if (produtos is null)
+            {
+                throw new NullReferenceException(nameof(produtos));
+            }
+
+            return produtos;
+
+        }
+
+        public Produto CreateProdutos(Produto produto)
+        {
+            throw new NotImplementedException();
+        }
+
+        public bool DeleteProduto(int id)
+        {
+            throw new NotImplementedException();
+        }
+
+        public Produto GetProduto(int id)
+        {
+            throw new NotImplementedException();
+        }
+
+
+        public bool UpdateProduto(Produto produto)
+        {
+            throw new NotImplementedException();
+        }
+    }
+}
